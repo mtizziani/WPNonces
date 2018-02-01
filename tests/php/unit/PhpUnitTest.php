117ef3d1027f6f2201f775ad60934dd289a89c3c
Maik Tizziani <mtizziani@gmail.com>
2018-02-01 22:18:27 +0100

    add test to check if phpunit runs in ide

---
+++ tests/php/unit/PhpUnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mtizziani\WPNonces\Tests\Php\Unit;
+
+
+class PhpUnitTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function check_if_phpunit_runs_correct(){
+        $this->assertTrue(true);
+    }
+}
