19eb1b9dc10a6bb900b407e5ba553f122a50f988
Maik Tizziani <mtizziani@gmail.com>
2018-02-01 23:36:26 +0100

    add test for WPNonce Class

---
+++ tests/php/unit/WPNonceTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maik
+ * Date: 01.02.18
+ * Time: 22:22
+ */
+
+namespace Mtizziani\WPNonces\Tests\php\unit;
+
+use Mtizziani\WPNonces\{
+    Nonces as NonceRoot
+};
+
+class WPNonceTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function If_Static_Test_Function_Returns_True() {
+        $result = NonceRoot::test();
+        $this->assertTrue($result);
+    }
+}
