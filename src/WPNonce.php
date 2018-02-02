149cf87e7a3685843f039244e0627e1edf24fec4
Maik Tizziani <mtizziani@gmail.com>
2018-02-02 02:48:42 +0100

    adds method description

--- src/WPNonce.php
+++ src/WPNonce.php
@@ -10,6 +10,13 @@ namespace Mtizziani\WPNonces;
  */
 class WPNonce
 {
+    protected $nonce = '';
+
+    /**
+     * a simple static function for testing if WPNonce can be called
+     *
+     * @return bool always return true
+     */
     public static function test() {
         return true;
     }
