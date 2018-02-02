0e2bb1ea1cd423ac48b136fbd3eefdb096d51c1b
Maik Tizziani <mtizziani@gmail.com>
2018-02-02 04:17:56 +0100

    create methods for setting action and create a nonce
    - created protected property action => default ''
    - created protected property nonce => default ''
    - created method action(...) => setter and getter for action
    - created method nonce(...) => creates a nonce if action is set and null the nonce if action is not set

--- src/WPNonce.php
+++ src/WPNonce.php
@@ -3,13 +3,21 @@
 namespace Mtizziani\WPNonces;
 
 /**
- * Class Nonces
+ * Class WPNonce
  *
- * @author Maik Tizziani <mtizziani@gmail.com>
  * @package Mtizziani\WPNonces
  */
 class WPNonce
 {
+
+    /**
+     * @var string
+     */
+    protected $action = '';
+
+    /**
+     * @var string
+     */
     protected $nonce = '';
 
     /**
@@ -20,4 +28,48 @@ class WPNonce
     public static function test() {
         return true;
     }
+
+    /**
+     * getter and setter for action property
+     *
+     * if isset $actionName and $actionName is not an empty string after trim
+     *   => property action will be set
+     * else
+     *   => returns an empty string
+     *
+     * @param string|NULL $actionName
+     * @return string
+     */
+    public function action(string $actionName = NULL): string {
+        if(!is_null($actionName)) {
+            $actionName = trim($actionName);
+            if(strlen($actionName) > 0) {
+                $this->action = $actionName;
+            }
+        }
+        return $this->action;
+    }
+
+    /**
+     * create a nonce and get it back
+     *
+     * if isset $actionName
+     *   => property action will also be set
+     *
+     * if property action is not empty
+     *   => nonce will be created
+     *
+     * @param string|NULL $actionName
+     * @return string
+     */
+    public function nonce(string $actionName = NULL): string {
+        $action = $this->action($actionName);
+        $value = '';
+        if(strlen($action) > 0) {
+            $value = wp_create_nonce($action);
+        }
+        $this->nonce = $value;
+        return $this->nonce;
+    }
+
 }
